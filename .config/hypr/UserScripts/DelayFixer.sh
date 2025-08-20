d0cdc057b550ecd2b5bcd3d0133b71acf725e734
André Jorge Lopes <andrelopes.professional@gmail.com>
2025-08-20 02:51:47 +0100

    Tries to fix caps lock delay and adds config for vesktop app

--- .config/hypr/UserScripts/DelayFixer.sh
+++ .config/hypr/UserScripts/DelayFixer.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
+#!/bin/sh
+rpl='key <CAPS> {\
+repeat=no,\
+type[group1]="ALPHABETIC",\
+symbols[group1]=[ Caps_Lock, Caps_Lock],\
+actions[group1]=[ LockMods(modifiers=Lock),\
+Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
+}'
 
-sleep 2
+# Create copy of kb description
+xkbcomp -xkb $DISPLAY keyboardmap
 
-if [ -n "$DISPLAY" ]; then
-  xkbcomp -w 0 ./xkbmap $DISPLAY
-  echo "CapsFix ran at $(date)" >>/tmp/capsfix.log
-fi
+# Replace CAPS
+sed -i "s/key <CAPS>[^;]*/$rpl/" keyboardmap
+
+# Apply
+xkbcomp -w 0 keyboardmap $DISPLAY
+
+# Remove temp file
+rm keyboardmap
